97dfc6e17945f43cbe933f1b60a96b29021bcea0
tangdengbo.tony <tangling11@163.com>
2019-09-28 11:39:43 +0800

    feat: change auto_changelog.sh to add new command
    add new package install

--- auto_changelog.sh
+++ auto_changelog.sh
@@ -4,4 +4,5 @@
 git pull
 git fetch origin
 sudo npm install -g conventional-changelog-cli
-conventional-changelog -p angular -i CHANGELOG.md -s -p
+sudo npm install -g conventional-changelog
+conventional-changelog -p atom -i CHANGELOG.md -s
