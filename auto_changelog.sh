6afdf83ae273c7a92ebcf130afd817aa0e37c457
tangdengbo.tony <tangling11@163.com>
2019-09-28 11:16:48 +0800

    feat: add post commit shell to auto changelog
     - modify auto_changelog.sh add tail to markdown file
     - modify docs CHANGELOG.md delete extra information
     - add new file that can auto generate CHANGELOG.md when a commit push

--- auto_changelog.sh
+++ auto_changelog.sh
@@ -4,4 +4,4 @@
 git pull
 git fetch origin
 sudo npm install -g conventional-changelog-cli
-conventional-changelog -p angular -i CHANGELOG.md -s
+conventional-changelog -p angular -i CHANGELOG.md -s -p
